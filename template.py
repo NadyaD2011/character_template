27bc4be4503f1adaf1e060af4ae06783b2d63091
NadyaEvp <t_evp@mail.ru>
2025-06-18 18:03:48 +0300

    Update template.py

--- template.py
+++ template.py
@@ -4,81 +4,113 @@ import random
 import os
 
 
-def rendered_page(template, races, classes):
+def create_card(races, classes):
     clases_base = {
         'Волшебник': {
-            'strength' : random.randint(1, 3),
-            'agility' : random.randint(1, 3),
-            'intelligence' : 15,
-            'luck' : random.randint(1, 3),
-            'temper' : random.randint(1, 3),
-            'skills': ['Стрела ледяного огня', 'Снятие проклятия', 'Огненный взрыв', 'Обледенение', 'Ледяное копье', 'Конус холода', 'Прилив сил', 'Морозный доспех'],
+            'strength': random.randint(1, 3),
+            'agility': random.randint(1, 3),
+            'intelligence': 15,
+            'luck': random.randint(1, 3),
+            'temper': random.randint(1, 3),
+            'skills': [
+                'Стрела ледяного огня',
+                'Снятие проклятия',
+                'Огненный взрыв',
+                'Обледенение',
+                'Ледяное копье',
+                'Конус холода',
+                'Прилив сил',
+                'Морозный доспех'
+            ],
             'img': 'images/wizard.png',
         },
         'Воин': {
-            'strength' : 15,
-            'agility' : random.randint(1, 3),
-            'intelligence' : random.randint(1, 3),
-            'luck' : random.randint(1, 3),
-            'temper' : random.randint(1, 3),
-            'skills': ['Блок щитом', 'Казнь', 'Рывок', 'Боевой крик', 'Вихрь', 'Парирование', 'Мощный удар', 'Глубокие раны'],
+            'strength': 15,
+            'agility': random.randint(1, 3),
+            'intelligence': random.randint(1, 3),
+            'luck': random.randint(1, 3),
+            'temper': random.randint(1, 3),
+            'skills': [
+                'Блок щитом',
+                'Казнь',
+                'Рывок',
+                'Боевой крик',
+                'Вихрь',
+                'Парирование',
+                'Мощный удар',
+                'Глубокие раны'
+            ],
             'img': 'images/warrior.png',
         },
         'Бард': {
-            'strength' : random.randint(1, 3),
-            'agility' : random.randint(1, 3),
-            'intelligence' : random.randint(1, 3),
-            'luck' : random.randint(1, 3),
-            'temper' : 15,
-            'skills': ['Аккорды ветра', 'Аккорды воды', 'Исцеление', 'Соната жизни', 'Пауза', 'Плач сирен', 'Песнь ветра', 'Реквием'],
+            'strength': random.randint(1, 3),
+            'agility': random.randint(1, 3),
+            'intelligence': random.randint(1, 3),
+            'luck': random.randint(1, 3),
+            'temper': 15,
+            'skills': [
+                'Аккорды ветра',
+                'Аккорды воды',
+                'Исцеление',
+                'Соната жизни',
+                'Пауза',
+                'Плач сирен',
+                'Песнь ветра',
+                'Реквием'
+            ],
             'img': 'images/bard.webp',
         },
         'Ассасин': {
-            'strength' : random.randint(1, 3),
-            'agility' : random.randint(1, 3),
-            'intelligence' : random.randint(1, 3),
-            'luck' : 15,
-            'temper' : random.randint(1, 3),
-            'skills': ['Отравление', 'Взлом замка', 'Подлый трюк', 'Исчезновение', 'Ложный выпад', 'Внезапный удар', 'Ошеломление', 'Спринт'],
+            'strength': random.randint(1, 3),
+            'agility': random.randint(1, 3),
+            'intelligence': random.randint(1, 3),
+            'luck': 15,
+            'temper': random.randint(1, 3),
+            'skills': [
+                'Отравление',
+                'Взлом замка',
+                'Подлый трюк',
+                'Исчезновение',
+                'Ложный выпад',
+                'Внезапный удар',
+                'Ошеломление',
+                'Спринт'
+            ],
             'img': 'images/assasin.png',
         },
         'Лучник': {
-            'strength' : random.randint(1, 3),
-            'agility' : 15,
-            'intelligence' : random.randint(1, 3),
-            'luck' : random.randint(1, 3),
-            'temper' : random.randint(1, 3),
-            'skills': ['Верный выстрел', 'Чародейский выстрел', 'Стенающая стрела', 'Стрелы ветра', 'Призыв питомца', 'Глаз зверя', 'Осветительная ракета', 'Приручение животного'],
+            'strength': random.randint(1, 3),
+            'agility': 15,
+            'intelligence': random.randint(1, 3),
+            'luck': random.randint(1, 3),
+            'temper': random.randint(1, 3),
+            'skills': [
+                'Верный выстрел',
+                'Чародейский выстрел',
+                'Стенающая стрела',
+                'Стрелы ветра',
+                'Призыв питомца',
+                'Глаз зверя',
+                'Осветительная ракета',
+                'Приручение животного'
+            ],
             'img': 'images/archer.png',
         },
     }
 
-    character_name = input('Введите имя персонажа: ')
-    character_race = int(input(f'Введите номер расы от 1 до {len(races)}(расы: {races}): '))
+    character_name = input('Введите имя персонажа: ').capitalize()
+    character_race = races[int(input(f'Введите номер расы от 1 до {len(races)}(расы: {races}): '))-1]
     character_class = classes[(int(input(f'Введите номер класса от 1 до {len(classes)}(классы: {classes}): ')))-1]
     skills = random.sample(clases_base[character_class]['skills'], 3)
 
-    rendered_page = template.render(
-        name = character_name.capitalize(),
-        race = races[character_race-1],
-        character_class = character_class,
-        image = clases_base[character_class]['img'],
-        strength = clases_base[character_class]['strength'],
-        agility = clases_base[character_class]['agility'],
-        intelligence = clases_base[character_class]['intelligence'],
-        luck = clases_base[character_class]['luck'],
-        temper = clases_base[character_class]['temper'],
-        first_skill = skills[0],
-        second_skill = skills[1],
-        third_skill = skills[2],
-    )
+    character_date = {
+        'name': character_name,
+        'race': character_race,
+        'class': character_class,
+        'skills': skills
+    }
 
-    return rendered_page
-
-
-def create_card(num, dir_name, rendered_page):
-    with open(f'{dir_name}/index_{num+1}.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    return character_date, clases_base
 
 
 def main():
@@ -88,8 +120,19 @@ def main():
     )
     template = env.get_template('template.html')
 
-    races = ['Орк', 'Хоббит', 'Эльф', 'Гном']
-    classes = ['Волшебник', 'Воин', 'Бард', 'Ассасин', 'Лучник']
+    races = [
+        'Орк',
+        'Хоббит',
+        'Эльф',
+        'Гном'
+    ]
+    classes = [
+        'Волшебник',
+        'Воин',
+        'Бард',
+        'Ассасин',
+        'Лучник'
+    ]
     dir_name = 'characters'
 
     os.makedirs(dir_name, exist_ok=True)
@@ -97,8 +140,24 @@ def main():
     quantity_cards = int(input("Сколько карточек нужно создать: "))
 
     for num in range(0, quantity_cards):
-        rendered_page = rendered_page(template, races, classes)
-        create_card(num, dir_name, rendered_page)
+        character, clases_base = create_card(template, races, classes)
+        character_class = character['class']
+        rendered_page = template.render(
+            name = character['name'],
+            race = character['race'],
+            character_class = character_class,
+            image = clases_base[character_class]['img'],
+            strength = clases_base[character_class]['strength'],
+            agility = clases_base[character_class]['agility'],
+            intelligence = clases_base[character_class]['intelligence'],
+            luck = clases_base[character_class]['luck'],
+            temper = clases_base[character_class]['temper'],
+            first_skill = character['skills'][0],
+            second_skill = character['skills'][1],
+            third_skill = character['skills'][2],
+        )
+        with open(f'{dir_name}/index_{num+1}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 if __name__ == "main":
